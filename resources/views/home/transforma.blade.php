fb1b9cf3aafa498f6d0a2ffaad45076016233bc2
Víctor Blasco <victor.blasco.17@gmail.com>
2025-12-28 12:55:52 +0100

    diseño navbar y vista trasforma en home

---
+++ resources/views/home/transforma.blade.php
@@ -0,0 +1,37 @@
+<style>
+    html,
+    body {
+        margin: 0;
+        overflow-x: hidden;
+    }
+
+    .img-home {
+        width: 100%;
+        height: 35em;
+        background-image: url('../images/transforma.png');
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        background-position: bottom;
+    }
+
+    .title-banner-home {
+        font-size: 60px;
+        color: white;
+        margin-top: 2em;
+    }
+
+    .subtitle-banner-home {
+        font-size: 25px;
+        color: white;
+        margin: 0;
+    }
+</style>
+
+<section>
+    <div class="img-home">
+        <h1 class="title-banner-home">TRASFORMA TU CUERPO</h1>
+        <p class="subtitle-banner-home"> Alcanza tus metas con nuestro equipo profesional</p>
+    </div>
+</section>
