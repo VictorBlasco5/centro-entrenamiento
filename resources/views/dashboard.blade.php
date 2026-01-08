93d229840bf09799c53cbefdd42b28934340c77d
Víctor Blasco <victor.blasco.17@gmail.com>
2026-01-08 10:45:18 +0100

    title calendar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centro de Entrenamiento</title>
+    <title>Calendario</title>
     <style>
         .container-calendar {
             font-family: var(--font-jost-regular);
